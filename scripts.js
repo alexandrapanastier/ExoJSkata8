ce511d12a001e3bd27dd6f945834b742b65bc99a
alexandrapanastier <panastier.alexandra@gmail.com>
2023-05-24 13:07:23 +0200

    first commit with four exercices

---
+++ scripts.js
@@ -0,0 +1,51 @@
+//----------------------------------------------------------------------------------------------//
+// EXERCICE 1
+// Crée une fonction qui prend deux paramètres (a et b) et retourne la somme multipliée des deux paramètres.
+// Exemple : a = 2, b = 3 => 2 * 3 = 6 => 6 * 2 = 12
+
+// Pour rappel pour écrire une fonction en JS on utilise la syntaxe suivante :
+
+// const nomDeMaFonction = (param1, param2) => {
+//   // Code de ma fonction
+// }
+// console.log(nomDeMaFonction(2, 3)) // 
+
+// CODE ICI
+
+//----------------------------------------------------------------------------------------------//
+// EXERCICE 2
+
+// Crée une fonction qui prend en paramètre un tableau et qui retourne le premier élément du tableau.
+// Exemple : [1, 2, 3] => 1
+// Pour rappel pour accéder à un élément d'un tableau on utilise la méthode [laplaceDuNombreDeL'élément] :
+// const monTableau = [1, 2, 3]
+// console.log(monTableau[0]) // 1
+
+// CODE ICI
+
+//----------------------------------------------------------------------------------------------//
+// EXERCICE 3
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit supprimer le dernier élément du tableau.
+// Exemple : [1, 2, 3] => [1, 2]
+// Pour rappel pour supprimer le dernier élément d'un tableau on utilise la méthode pop():
+// const monTableau = [1, 2, 3]
+// monTableau.pop()
+// console.log(monTableau) // [1, 2]
+
+// CODE ICI
+
+//----------------------------------------------------------------------------------------------//
+// EXERCICE 4
+
+// Crée une fonction qui prend en paramètre un tableau comprennant des nombres et qui doit retourner la somme de tous les nombres du tableau.
+// Exemple : [1, 2, 3] => 6
+// Pour rappel pour parcourir un tableau on utilise la méthode for :
+// const monTableau = [1, 2, 3]
+// for (let i = 0; i < monTableau.length; i++) {
+//   console.log(monTableau[i]) // 1, 2, 3
+// }
+
+// CODE ICI
+
+//----------------------------------------------------------------------------------------------//
