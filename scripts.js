ca7757a9990729b45ededf64311cfc1ad7f522a4
alexandrapanastier <panastier.alexandra@gmail.com>
2023-12-14 09:22:08 +0100

    News exercices algo

--- scripts.js
+++ scripts.js
@@ -743,3 +743,116 @@ sumArr( ["2", "5", "3"], ["2", "4", "9", "5", "5"] ) should return ["4", "9", "1
 // }
 
 // console.log(sumArr(["1", "2", "3"], ["2", "4", "1"]))
+
+//----------------------------------------------------------------------------------------------//
+
+// EXERCICE 32
+
+// Écris une fonction generatedCharacter qui crée de manière aléatoires des personnages donjon et dragon avec une classe et des noms de personnages. La fonction doit renvoyer un tableau de personnages.
+// Exemple du résultat attendu :
+// [
+//   ["Aldric", "barbarian"],
+//   ["Dolgrin", "bard"],
+//   ["Ernst", "cleric"],
+//   ["Gellius", "druid"],
+//   ["Gorstag", "fighter"]
+// ]
+
+// Indice : Tu dois créer un tableau vide et le remplir avec des tableaux qui contiennent un nom et une classe. Pour créer un nom aléatoire tu dois créer un tableau de noms et utiliser la méthode Math.random() pour récupérer un nom aléatoire dans le tableau de noms. Pour créer une classe aléatoire tu dois créer un tableau de classes et utiliser la méthode Math.random() pour récupérer une classe aléatoire dans le tableau de classes. Et tu dois faire une boucle pour créer 10 personnages.
+
+// CODE ICI
+
+let tableauName = ["Aldric", "Dolgrin", "Ernst", "Gellius", "Gorstag", "Hagar", "Jozan", "Kef", "Leve", "Morn", "Perrin", "Reed", "Rulf", "Shandar", "Taman", "Urth"]
+let tableauClass = ["barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------SOLUTIONS-----------------------------------------------//
+// const generatedCharacter= () => {
+//     let tableau = []
+//     for (let i = 0; i < 10; i++) {
+//         let tableau2 = []
+//         let randomName = tableauName[Math.floor(Math.random() * tableauName.length)]
+//         let randomClass = tableauClass[Math.floor(Math.random() * tableauClass.length)]
+//         tableau2.push(randomName)
+//         tableau2.push(randomClass)
+//         tableau.push(tableau2)
+//     }
+//     return tableau
+// }
+
+// console.log(generatedCharacter())
+
+
+//-----------------------------------------------SOLUTIONS-----------------------------------------------//
+
+// Explication ici nous allons résoudre l'algo avec un .map(). Pourquoi ? Car le .map() permet de parcourir un tableau et de retourner un nouveau tableau avec les éléments modifiés. Donc ici nous allons parcourir le tableauName avec un .map() et pour chaque élément du tableauName nous allons créer un tableau avec un nom aléatoire et une classe aléatoire. Et nous allons retourner ce tableau. Donc le .map() va créer un nouveau tableau avec des tableaux qui contiennent un nom aléatoire et une classe aléatoire. Et nous allons stocker ce nouveau tableau dans une variable tableau. Et nous allons retourner ce tableau à la fin de la fonction.
+
+// Comme vous pouvez le voir il y a plusieurs façon de réussir à avoir le résultat attendu d'un algo. Il y a pas de bonne ou de mauvaise façon de faire. Il y a juste des façons plus optimisées que d'autres. Et il y a des façons plus simples que d'autres. Mais le plus important c'est de réussir à trouver une solution qui fonctionne. Et si vous avez réussi à trouver une solution qui fonctionne, c'est déjà très bien. 
+
+// const generatedCharacter = () => {
+//     let tableau = []
+//     tableauName.map((element) => {
+//         let tableau2 = []
+//         let randomClass = tableauClass[Math.floor(Math.random() * tableauClass.length)]
+//         tableau2.push(element)
+//         tableau2.push(randomClass)
+//         tableau.push(tableau2)
+//     })
+//     return tableau
+// }
+
+// console.log(generatedCharacter())
+
+//----------------------------------------------------------------------------------------------//
+
+// EXERCICE 33
+
+//Écrivez une fonction nommée "findMissingLetter" qui prend en paramètre un tableau d'une série de lettres (en tant que chaînes de caractères) et qui renvoie la lettre manquante de la série. Si aucune lettre n'est manquante, la fonction doit renvoyer "undefined".
+// Exemple :
+// findMissingLetter(["a","b","c","d","f"]) // "e"
+// findMissingLetter(["O","Q","R","S"]) // "P"
+
+//Indice : Oubliez pas la méthode charCodeAt() est utile pour récupérer le code unicode d'un caractère.
+//Le unicode est une norme informatique qui permet de représenter des caractères de toutes les langues du monde. Chaque caractère possède un code unicode qui lui est propre. Par exemple, le code unicode de "A" est 65, le code unicode de "B" est 66, le code unicode de "C" est 67, etc. Pour récupérer le code unicode d'un caractère vous devez utiliser la méthode charCodeAt(). Cette méthode prend en paramètre l'index du caractère dans la string. Par exemple, si vous voulez récupérer le code unicode du premier caractère de la string vous devez utiliser la méthode charCodeAt(0). Si vous voulez récupérer le code unicode du deuxième caractère de la string vous devez utiliser la méthode charCodeAt(1). Et ainsi de suite.
+
+
+
+// CODE ICI
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------SOLUTIONS-----------------------------------------------//
+
+
+// const findMissingLetter = (tableau) => {
+//     let tableauUnicode = tableau.map((element) => {
+//         return element.charCodeAt()
+//     })
+//     for (let i = 0; i < tableauUnicode.length; i++) {
+//         if (tableauUnicode[i] !== tableauUnicode[i + 1] - 1) {
+//             return String.fromCharCode(tableauUnicode[i] + 1)
+//         }
+//     }
+// }
+
+// console.log(findMissingLetter(["a","b","c","d","f"]))
